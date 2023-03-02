a9f99f45f5d25f67f6ce89e5d3293c565f5a87cb
khameleonstorm <khameleonstorm@gmail.com>
2023-03-02 01:05:01 +0100

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // assetPrefix: 'production' ? 'https://cdn.mydomain.com' : undefined,
 }
 
 module.exports = nextConfig
