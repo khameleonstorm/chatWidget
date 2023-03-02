0d86113e523ac474c76fa679fc73813ff79a1446
khameleonstorm <khameleonstorm@gmail.com>
2023-03-02 01:56:27 +0100

    CDN

--- .next/static/iIvwdyOmwhg1PNOQ7uBeg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95b64a6e-cfba3a3ed255356b.js","static/css/253dde97e60d1f0f.css","static/chunks/pages/index-3ba4fd4f0e1e0065.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
